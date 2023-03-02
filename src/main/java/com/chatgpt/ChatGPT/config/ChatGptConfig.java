e721c9f9cb2e029e7e3318c53cdadbe9284b5221
Md Yaseen Rehman <yaseen.r@royalcyber.com>
2023-03-02 18:56:03 +0530

    Modified code

--- src/main/java/com/chatgpt/ChatGPT/config/ChatGptConfig.java
+++ src/main/java/com/chatgpt/ChatGPT/config/ChatGptConfig.java
@@ -4,7 +4,7 @@ public class ChatGptConfig {
 
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "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";
+    public static final String API_KEY = "";
     public static final String MODEL = "text-davinci-003";
     public static final Integer MAX_TOKEN = 300;
     public static final Double TEMPERATURE = 0.0;
